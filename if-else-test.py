90790ff599b035e23c07ebd963fc82edf6ef130a
ajinkyadjoshi <ajinkya.joshi.d@hotmail.com>
2015-06-20 23:30:54 +0530

    if-else

---
+++ if-else-test.py
@@ -0,0 +1,8 @@
+port = input("Enter the port number: " )
+if port == 21:
+    print ("FTP to the site")
+elif port == 22:
+    print ("SSH to the site")
+else:
+    print ("Make some another request")
+print ("Done!")
