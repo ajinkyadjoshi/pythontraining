3814e13e7cff6b26f6adc7575c20014db64752a0
ajinkyadjoshi <ajinkya.joshi.d@hotmail.com>
2015-06-21 11:06:29 +0530

    Day1-next part

---
+++ Day1/Basic-Math.py
@@ -0,0 +1,3 @@
+print ("Addition of 2 and 2 is: ", 2 + 2)
+
+print ((3 *4 ) / 6)
---
+++ Day1/variable_declaration.py
@@ -0,0 +1,16 @@
+x = "Ajinkya"   # comment
+y = 22.90
+z = 12
+
+print ("x is : ", x)
+print ("y is : ", y)
+print ("z is : ", z)
+
+# Multiline statements
+
+print("x is : ", \
+       x)
+
+#Error
+# days = ['Monday', 'Tuesday', 'Wednesday',
+#         'Thursday', 'Friday']
