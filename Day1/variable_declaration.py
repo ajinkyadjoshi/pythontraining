9034908d14ef4433d87ced377c1a8e61211720ff
ajinkyadjoshi <ajinkya.joshi.d@hotmail.com>
2015-06-21 12:35:05 +0530

    Day1

--- Day1/variable_declaration.py
+++ Day1/variable_declaration.py
@@ -14,3 +14,14 @@ print("x is : ", \
 #Error
 # days = ['Monday', 'Tuesday', 'Wednesday',
 #         'Thursday', 'Friday']
+
+#Quotation in Python
+
+# single -> 'Hello World'
+# double -> "Hello World"
+# triple -> """ This is a paragraph """
+
+para = """This is a paragraph. It is
+made up of multiple lines and sentences."""
+
+print(para)
